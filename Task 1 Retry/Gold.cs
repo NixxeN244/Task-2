cf40a5b906b023a289ae51594d2fce525f8f2f4a
NixxeN244 <ns233282@gmail.com>
2020-10-11 10:32:52 +0200

    Creation of the item class and the Gold class

---
+++ Task 1 Retry/Gold.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Retry
+{
+    class Gold : Item
+    {
+
+        private int goldAmount;
+        private Random randomAmount = new Random();
+        public Gold(int X, int Y) : base(X, Y)
+        {
+            GoldAmount = randomAmount.Next(1, 6);
+        }
+
+        public int GoldAmount
+        {
+            get { return goldAmount; }
+            set { goldAmount = value; }
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Task 1 Retry/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_Retry
+{
+   abstract class Item: Tile
+    {
+        public Item(int X, int Y) : base(X, Y)
+        {
+
+        }
+
+        public abstract override string ToString();
+    }
+}
